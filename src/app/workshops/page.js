a47cb72149d592180dd129fa8360371a4c94daa0
Simon <62551180+simonagleev@users.noreply.github.com>
2024-12-15 17:54:26 +0300

    mobile workshops styles

--- src/app/workshops/page.js
+++ src/app/workshops/page.js
@@ -13,10 +13,9 @@ export default function Workshops() {
                         <h1 className={styles.header}>МАСТЕР</h1>
                         <h1 className={styles.header}>КЛАССЫ</h1>
                     </div>
-
                 </div>
                 {/* <div className={styles.payment_card}> */}
-                    <PaymentCardCommon type={'mk'} />
+                <PaymentCardCommon type={'mk'} />
                 {/* </div> */}
             </div>
             <div className={styles.content}>
