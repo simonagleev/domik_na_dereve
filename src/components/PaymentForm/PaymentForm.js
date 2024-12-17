92ab80d9900fc4849235e56020d51a56fdf9c272
Simon <62551180+simonagleev@users.noreply.github.com>
2024-12-17 18:47:41 +0300

    payment working

--- src/components/PaymentForm/PaymentForm.js
+++ src/components/PaymentForm/PaymentForm.js
@@ -2,7 +2,7 @@
 import styles from "./PaymentForm.module.css";
 import { usePaymentModalStore } from "@/store/PaymentModalStore";
 import axios from 'axios';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function PaymentForm({ type, data }) {
     const {
@@ -24,7 +24,7 @@ export default function PaymentForm({ type, data }) {
             updateCount((prevCount) => {
                 const newCount = prevCount - 1;
                 updateFormData('amount', newCount * data.Price);
-    
+
                 return newCount; // Возвращаем обновленное состояние
             })
         } else {
@@ -33,11 +33,11 @@ export default function PaymentForm({ type, data }) {
     }
 
     const handleIncrement = () => {
-        if(count < data.RemainingCount) {
+        if (count < data.RemainingCount) {
             updateCount((prevCount) => {
                 const newCount = prevCount + 1;
                 updateFormData('amount', newCount * data.Price);
-        
+
                 return newCount;
             })
         } else (
@@ -50,13 +50,12 @@ export default function PaymentForm({ type, data }) {
         updateFormData('type', type)
         updateFormData('date', data.StartDateTime)
         updateFormData('itemID', data.ID)
+        updateFormData('amount', count * data.Price);
+
     }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-
-
         // try {
         //     // Отправка данных в Telegram через API
         //     await axios.post('/api/sendFormData', formData);
@@ -68,8 +67,56 @@ export default function PaymentForm({ type, data }) {
         // }
     };
 
+    const handlePhoneChange = (e) => {
+        let value = e.target.value;
+        // Убираем все символы, кроме цифр и знака +
+        value = value.replace(/[^0-9+]/g, '');
+        // Если это первый символ и он не равен +, заменяем его на +7
+        if (value && value[0] !== '+') {
+            value = `+7${value.slice(1)}`; // Заменяем первый символ на +7
+        }
+        updateFormData('phone', value);
+    };
+
+    const [loading, setLoading] = useState(false);
+
+    const handlePayment = async () => {
+        console.log('PAYMENT STARTED')
+        setLoading(true);
+
+        try {
+            const response = await fetch('/api/create-payment', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    amount: formData.amount,
+                    description: 'Оплата заказа',
+                    return_url: type === 'show' ? 'http://localhost:3000/shows' : 'https://domiknadereve-irk.ru', //Доделать, прописть все ссылки на проде потом
+                    phone: formData.phone,
+                    itemID: formData.itemID,
+                    type: type,
+                    info: formData.info,
+                }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok && data.confirmationUrl) {
+                console.log('COOL')
+                window.location.href = data.confirmationUrl; // Редирект на ЮKassa
+            } else {
+                console.error('Ошибка при получении ссылки на оплату:', data);
+            }
+        } catch (error) {
+            console.error('Ошибка при запросе на сервер:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div className={styles.modal_overlay} onClick={handleClickOutside}>
+            {!loading ? 
             <div className={styles.payment_form}>
                 {/* Кнопка крестик */}
                 <div className={styles.close_button_container}>
@@ -79,7 +126,7 @@ export default function PaymentForm({ type, data }) {
                 </div>
 
                 <h2 className={styles.payment_form_heading}>Заполните форму для записи</h2>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handlePayment}>
                     <div className={styles.form_group}>
                         <input type="text" id="name" name="name" placeholder="Имя ребенка"
                             value={formData.name || ''}
@@ -88,7 +135,7 @@ export default function PaymentForm({ type, data }) {
                     <div className={styles.form_group}>
                         <input id="phone" name="phone" placeholder="Номер телефона для связи"
                             value={formData.phone || ''}
-                            onChange={(e) => updateFormData('phone', e.target.value)} />
+                            onChange={(e) => handlePhoneChange(e)} />
                     </div>
                     <div className={styles.form_group}>
                         <input type="email" id="email" name="email" placeholder="Email"
@@ -121,7 +168,7 @@ export default function PaymentForm({ type, data }) {
                 <h2 className={styles.payment_form_heading}>
                     +7 (333) 355-44-77
                 </h2>
-            </div>
+            </div> : <div>LOADING</div>}
         </div>
     );
 }
