5461797b30358bc75fc4b3c8ae79c28fcb161c5a
Simon <62551180+simonagleev@users.noreply.github.com>
2024-12-17 19:37:12 +0300

    test

---
+++ src/app/api/check-env/route.js
@@ -0,0 +1,12 @@
+export async function GET() {
+    return new Response(
+      JSON.stringify({
+        shopId: process.env.YOOKASSA_SHOP_ID || "Shop id not found",
+        secretKey: process.env.YOOKASSA_SECRET_KEY || "Key not found",
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      }
+    );
+  }
