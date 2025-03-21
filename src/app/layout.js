d61ce17eeb4d60e358cfc49f6bb1eaa318bd0f31
Simon <62551180+simonagleev@users.noreply.github.com>
2025-03-21 17:16:54 +0300

    yandex metrika

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import FooterTree from "@/components/FooterTree/FooterTree";
 import Header from "@/components/Header/Header";
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/next';
+import Script from "next/script";
 
 export const metadata = {
   title: "Домик на дереве",
@@ -20,8 +21,41 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
         <FooterTree />
         <Footer />
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
         <Analytics />
+        {/* Яндекс.Метрика */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+              k.async=1;k.src=r;a.parentNode.insertBefore(k,a);
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(100456556, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });`,
+          }}
+        />
+
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/100456556"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
